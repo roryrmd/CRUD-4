ca448846cd72056ca2e37bbc58e6209f804ba9dc
roryrmd <rorydianr@gmail.com>
2021-07-15 11:24:42 +0700

    Adding Models and DOA's file of Regions and Employees

---
+++ Kelompok_4/src/daos/EmployeeDAO.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import models.Employee;
+
+public class EmployeeDAO {
+    private final Connection connection;
+
+    public EmployeeDAO(Connection connection) {
+        this.connection = connection;
+    }
+    
+    public List<Employee> getAll() {
+        List<Employee> employees = new ArrayList<Employee>();
+        String query = "SELECT * FROM EMPLOYEES ORDER BY Employee_id";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet                 = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Employee e = new Employee();
+                e.setEmployee_id (resultSet.getInt(1));
+                e.setLast_name   (resultSet.getString(2));
+                employees.add(e);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return employees;
+    }
+      
+    public boolean insert(Employee employee) {
+        boolean result = false;
+        String query = "INSERT INTO EMPLOYEES(employee_id, first_name, "
+                     + "last_name, email, phone_number, hire_date, job_id, "
+                     + "salary, commission_pct, manager_id, department_id) "
+                     + "VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt    (1, employee.getEmployee_id());
+            preparedStatement.setString (2, employee.getFirst_name());
+            preparedStatement.setString (3, employee.getLast_name());
+            preparedStatement.setString (4, employee.getEmail());
+            preparedStatement.setInt    (4, employee.getPhone_number());
+            preparedStatement.setDate   (6, employee.getHire_date());
+            preparedStatement.setInt    (7, employee.getJob_id());
+            preparedStatement.setDouble (8, employee.getSalary());
+            preparedStatement.setDouble (9, employee.getCommission_pct());
+            preparedStatement.setInt    (10, employee.getManager_id());
+            preparedStatement.setInt    (11, employee.getDepartment_id());
+            
+            preparedStatement.executeQuery();
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+            }
+        return result;
+    }
+    
+    public boolean update(Employee employee) {
+        boolean result = false;
+        String query = "UPDATE EMPLOYEES SET first_name =?, last_name =?, email =?, "
+                     + "phone_number =?, hire_date =?, job_id =?, salary =?, commission_pct =?, "
+                     + "manager_id =?, department_id =? WHERE employee_id =? ";
+        
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString (1, employee.getFirst_name());
+            preparedStatement.setString (2, employee.getLast_name());
+            preparedStatement.setString (3, employee.getEmail());
+            preparedStatement.setInt    (4, employee.getPhone_number());
+            preparedStatement.setDate   (5, employee.getHire_date());
+            preparedStatement.setInt    (6, employee.getJob_id());
+            preparedStatement.setDouble (7, employee.getSalary());
+            preparedStatement.setDouble (8, employee.getCommission_pct());
+            preparedStatement.setInt    (9, employee.getManager_id());
+            preparedStatement.setInt    (10, employee.getDepartment_id());
+            preparedStatement.setInt    (11, employee.getEmployee_id());
+            preparedStatement.executeQuery();
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+            }
+        return result;
+    }
+    
+    public boolean delete(Employee employee) {
+        boolean result = false;
+        String query = "DELETE FROM EMPLOYEES WHERE employee_id=? ";
+        
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, employee.getEmployee_id());
+            preparedStatement.executeQuery();
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+            }
+        return result;
+    }
+}
---
+++ Kelompok_4/src/daos/RegionDAO.java
@@ -0,0 +1,88 @@
+package daos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import models.Region;
+
+public class RegionDAO {
+
+    private final Connection connection;
+
+    public RegionDAO(Connection connection) {
+        this.connection = connection;
+    }
+    
+      public List<Region> getAll() {
+        List<Region> regions = new ArrayList<Region>();
+        String query = "SELECT * FROM REGIONS ORDER BY region_id";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Region r = new Region();
+                r.setId(resultSet.getInt(1));
+                r.setName(resultSet.getString(2));
+                regions.add(r);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return regions;
+    }
+      
+    public boolean insert(Region region) {
+        boolean result = false;
+        String query = "INSERT INTO REGIONS(region_name,region_id) VALUES(?,?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, region.getName());
+            preparedStatement.setInt(2, region.getId());
+            preparedStatement.executeQuery();
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+        }
+        return result;
+    }
+    
+    public boolean update(Region region) {
+        boolean result = false;
+        String query = "UPDATE REGIONS SET region_name =? WHERE region_id =? ";
+        
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, region.getName());
+            preparedStatement.setInt(2, region.getId());
+            preparedStatement.executeQuery();
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+        }
+        return result;
+        
+        }
+    
+    public boolean delete(Region region) {
+        boolean result = false;
+        String query = "DELETE FROM REGIONS WHERE region_id=? ";
+        
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, region.getId());
+            preparedStatement.executeQuery();
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+        }
+        return result;
+        
+        }
+
+    
+}
